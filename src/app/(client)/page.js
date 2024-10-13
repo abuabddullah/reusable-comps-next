35a106e05ccfd9109e823f7ba79176ded3ba2cae
asifaowadud <asifaowadud@gmail.com>
2024-10-13 13:10:02 +0600

    ReactInfiniteScroll starts now

--- src/app/(client)/page.js
+++ src/app/(client)/page.js
@@ -1,4 +1,4 @@
-import NextLevelImageComponent from "@/comps/NextImageEffects/NextLevelImageComponent";
+import ReactInfiniteScroll from "@/comps/ReactInfiniteScroll";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,11 +7,52 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <section>
-          <h1>This is the main section</h1>
-          <p className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-            Welcome to my Next.js &quot;Image Component&quot;
-          </p>
-          <NextLevelImageComponent />
+          <div>
+            <h1>This is the main section</h1>
+            <p className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Welcome to React-Infinite-Scroll
+            </p>
+            <p>
+              <small>
+                with{" "}
+                <strong>
+                  <a
+                    href="https://tanstack.com/query/latest/docs/framework/react/installation"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline underline-offset-4"
+                  >
+                    react-tanstack-query
+                  </a>
+                </strong>{" "}
+                ;{" "}
+                <strong>
+                  <a
+                    href="https://www.npmjs.com/package/react-intersection-observer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline underline-offset-4"
+                  >
+                    insertion-observer
+                  </a>
+                </strong>{" "}
+                ;{" "}
+              </small>
+            </p>
+            <p>
+              <small>
+                yt:{" "}
+                <a
+                  target="_blank"
+                  href="https://www.youtube.com/watch?v=DOKp4KiVIb4&ab_channel=CandDev"
+                  className="underline underline-offset-4"
+                >
+                  How to Create Infinite Scroll in React | TanStack React Query
+                </a>
+              </small>
+            </p>
+          </div>
+          <ReactInfiniteScroll />
         </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
---
+++ src/comps/ReactInfiniteScroll.jsx
@@ -0,0 +1,11 @@
+
+const ReactInfiniteScroll = () => {
+  return (
+    <div className='max-w-7xl mx-auto my-12 border p-8 rounded-lg shadow'>
+        <h1>React Infinite Scroll</h1>
+        
+    </div>
+  )
+}
+
+export default ReactInfiniteScroll

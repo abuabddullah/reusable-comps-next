e3aa9529a3bbeedc6621f760e24857637e4b0c84
asifaowadud <asifaowadud@gmail.com>
2024-10-16 11:02:07 +0600

    React Email with Resend starting

--- src/app/(client)/page.js
+++ src/app/(client)/page.js
@@ -1,58 +1,29 @@
-import ReactInfiniteScroll from "@/comps/ReactInfiniteScroll";
 import Image from "next/image";
 
 export default function Home() {
-  console.log("first");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <section>
           <div>
-            <h1>This is the main section</h1>
+            <p>This is the main section</p>
             <p className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Welcome to React-Infinite-Scroll
+              Welcome to Email with React & Resend mail
             </p>
-            <p>
-              <small>
-                with{" "}
-                <strong>
-                  <a
-                    href="https://tanstack.com/query/latest/docs/framework/react/installation"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="underline underline-offset-4"
-                  >
-                    react-tanstack-query
-                  </a>
-                </strong>{" "}
-                ;{" "}
-                <strong>
-                  <a
-                    href="https://www.npmjs.com/package/react-intersection-observer"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="underline underline-offset-4"
-                  >
-                    insertion-observer
-                  </a>
-                </strong>{" "}
-                ;{" "}
-              </small>
-            </p>
+            
             <p>
               <small>
                 yt:{" "}
                 <a
                   target="_blank"
-                  href="https://www.youtube.com/watch?v=DOKp4KiVIb4&ab_channel=CandDev"
+                  href="https://www.youtube.com/watch?v=btZII7TXlhk&ab_channel=CosdenSolutions"
                   className="underline underline-offset-4"
                 >
-                  How to Create Infinite Scroll in React | TanStack React Query
+                  How to React Email with Resend
                 </a>
               </small>
             </p>
           </div>
-          <ReactInfiniteScroll />
         </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
